1a941f84b1f4252928521b204020a5117e96d03d
55p <tempel.tuttle.55p@gmail.com>
2016-10-13 22:03:13 +0900

    fix .gitignore

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir -p $HOME/dotfiles/.vim/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git $HOME/dotfiles/.vim/dein/repos/github.com/Shougo/dein.vim
+
 ln -s $HOME/dotfiles/.bash_profile $HOME
 ln -s $HOME/dotfiles/.bashrc       $HOME
 ln -s $HOME/dotfiles/.tmux.conf    $HOME
@@ -9,3 +12,4 @@ ln -s $HOME/dotfiles/.vrapperrc    $HOME
 ln -s $HOME/dotfiles/.xvimrc       $HOME
 ln -s $HOME/dotfiles/.zprofile     $HOME
 ln -s $HOME/dotfiles/.zshrc        $HOME
+
